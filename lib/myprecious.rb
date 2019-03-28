c225fc592afc1bfa7c443799b32056288fc820ff
Balki <balki.kodarapu@gmail.com>
2019-03-28 11:39:15 -0700

    Update to use gem "gems" instead of scraping rubygems site

--- lib/myprecious.rb
+++ lib/myprecious.rb
@@ -1,8 +1,12 @@
 require 'gems'
 require 'date'
+require 'git'
 
 class MyPrecious
   def self.update
+    g = Git.open(Dir.pwd)
+    repo_name = g.repo.path.split(".git")[0].split("/").last
+    repo_name = repo_name + "-dependency-tracking"
     gem_lines = {}
     gem_name_pos = 0
     gem_version_pos = 1
@@ -12,11 +16,12 @@ class MyPrecious
     gem_license_pos = 5
     gem_change_pos = 6
     default_length = 7
-    if File.file?('myprecious.md')
-      File.open("myprecious.md", "r").each_with_index do |line, line_number|
+    already_fetched_gems = {}
+    if File.file?(repo_name+'.md')
+      File.open(repo_name+".md", "r").each_with_index do |line, line_number|
         #puts line + " dds " + line_number.to_s
         word_array = line.split('|')
-        if line_number == 0
+        if line_number == 1
           default_length = word_array.size
           word_array.each_with_index do |word, index|
             if word.strip == 'Gem'
@@ -28,25 +33,26 @@ class MyPrecious
             elsif word.strip == 'Date available'
               gem_date_pos = index
             elsif word.strip == 'Age (in days)'
-              gem_age_pos = index              
+              gem_age_pos = index
             elsif word.strip == 'License Type'
-              gem_change_pos = index              
+              gem_change_pos = index
             elsif word.strip == 'Change Log'
               gem_change_pos = index
             end
           end
-        elsif line_number > 1
+        elsif line_number > 2
           gem_name_index = word_array[gem_name_pos].strip
           #extract just the name of the gem from the first column
-          #since that column contains a markdown-formatted hyperlink  
+          #since that column contains a markdown-formatted hyperlink
           gem_name_index = gem_name_index[/\[(.*?)\]/,1]
           gem_lines[gem_name_index] = line_number
         end
-        
+
       end
       #puts gem_lines
     else
-      File.open('myprecious.md', 'w') { |write_file|
+      File.open(repo_name+'.md', 'w') { |write_file|
+        write_file.puts "Last updated:" + Date.today.to_s + "; Use for directional purposes only, this data is not real time and might be slightly inaccurate" + "\n\n"
         write_file.puts "Gem | Our Version | Latest Version | Date available | Age (in days) | License Type | Change Log"
         write_file.puts "--- | --- | --- | --- | --- | --- | ---"
       }
@@ -54,58 +60,62 @@ class MyPrecious
 
     file = File.new("Gemfile", "r")
 
-    final_write = File.readlines('myprecious.md')
+    final_write = File.readlines(repo_name+'.md')
 
     while (line = file.gets)
       gem_line = line.strip
       if (gem_line.include? 'gem') && !gem_line.start_with?('#') && !gem_line.start_with?('source')
+        name = gem_line.split(' ')[1].split(',')[0].tr(" '\"", "")
         begin
-          name = gem_line.split(' ')[1].split(',')[0].tr(" '\"", "")
-          a = require name
-          if a
-            current_version = Gem::Specification.find_all_by_name(name).max.version
-            
-            gems_info = Gems.versions name
-            gems_latest_info = Gems.info name
-            latest_build = ''
-            gems_info.each do |gem_info|
-              if gem_info["number"].to_s == gems_latest_info["version"].to_s
-                latest_build = Date.parse gem_info["built_at"]
-              end
-              if gem_info["number"].to_s == current_version.to_s
-                current_build = Date.parse gem_info["built_at"]
+          puts name + " is being fetched, and processed"
+          gems_latest_info = Gems.info name
+          current_version = Gem::Specification.find_all_by_name(name).max ? Gem::Specification.find_all_by_name(name).max.version : gems_latest_info["version"]
 
-                days_complete = latest_build - current_build
-                if gem_lines[name].nil?
-                  array_to_write = Array.new(default_length) { |i| "" }
-                else
-                  array_to_write = final_write[gem_lines[name]].split('|')
-                end
-                array_to_write[gem_name_pos] = "[" + name + "]" + "(" + gems_latest_info["homepage_uri"].to_s  + ")"
-                array_to_write[gem_version_pos] = current_version.to_s
-                array_to_write[gem_latest_pos] = gems_latest_info["version"].to_s
-                array_to_write[gem_date_pos] = (latest_build).to_s
-                array_to_write[gem_age_pos] = days_complete.to_i.to_s
-                if !gem_info["licenses"].nil?
-                  array_to_write[gem_license_pos] = gem_info["licenses"][0]
-                else
-                  array_to_write[gem_license_pos] = "N/A"
-                end
-                array_to_write[gem_change_pos] = gems_latest_info["changelog_uri"].to_s + "\n"
-                if !gem_lines[name].nil?
-                  final_write[gem_lines[name]] = array_to_write.join("|")
-                else                 
-                  final_write << array_to_write.join("|")
-                end
+         gems_info = Gems.versions name
+          latest_build = ''
+          gems_info.each do |gem_info|
+            if gem_info["number"].to_s == gems_latest_info["version"].to_s
+              latest_build = Date.parse gem_info["built_at"]
+            end
+            if gem_info["number"].to_s == current_version.to_s && !already_fetched_gems[name]
+              already_fetched_gems[name] = true
+              current_build = Date.parse gem_info["built_at"]
+
+              days_complete = latest_build - current_build
+              #puts name
+              #puts gem_lines
+              if gem_lines[name].nil?
+                array_to_write = Array.new(default_length) { |i| "" }
+              else
+                array_to_write = final_write[gem_lines[name]].split('|')
+              end
+              array_to_write[gem_name_pos] = "[" + name + "]" + "(" + gems_latest_info["homepage_uri"].to_s  + ")"
+              array_to_write[gem_version_pos] = current_version.to_s
+              array_to_write[gem_latest_pos] = gems_latest_info["version"].to_s
+              array_to_write[gem_date_pos] = (latest_build).to_s
+              array_to_write[gem_age_pos] = days_complete.to_i.to_s
+              if !gem_info["licenses"].nil?
+                array_to_write[gem_license_pos] = gem_info["licenses"][0]
+              else
+                array_to_write[gem_license_pos] = "N/A"
+              end
+              array_to_write[gem_change_pos] = gems_latest_info["changelog_uri"].to_s + "\n"
+              if !gem_lines[name].nil?
+                final_write[gem_lines[name]] = array_to_write.join("|")
+              else
+                final_write << array_to_write.join("|")
               end
             end
           end
         rescue Exception => e
           puts e
+          puts name
         end
       end
     end
-    File.open('myprecious.md', 'w') { |f| f.write(final_write.join) }
+    File.open(repo_name+'.md', 'w') { |f| f.write(final_write.join) }
     file.close
   end
 end
+
+MyPrecious.update
--- myprecious.gemspec
+++ myprecious.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'myprecious'
-  s.version     = '0.0.6'
-  s.date        = '2017-04-26'
+  s.version     = '0.0.8'
+  s.date        = '2019-03-28'
   s.summary     = "Your precious dependencies!"
   s.description = "A simple, markdown generated with information about your gems"
   s.authors     = ["Balki Kodarapu"]
@@ -9,6 +9,7 @@ Gem::Specification.new do |s|
   s.files       = ["lib/myprecious.rb"]
   s.executables << 'myprecious'
   s.add_runtime_dependency 'gems', '~> 1.0', '>= 1.0.0'
+  s.add_runtime_dependency 'git', '~> 1.5', '>= 1.5.0'
   s.homepage    =
     'http://rubygems.org/gems/myprecious'
   s.license       = 'MIT'
